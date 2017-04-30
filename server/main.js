9f6e2d39ca7fc8428a1ffc1bc393bed8efc55018
Kristian Elset Boe <kristian.e.boe@gmail.com>
2017-04-30 13:50:53 +0200

    Moved some code to the server

--- server/main.js
+++ server/main.js
@@ -3,3 +3,30 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+Meteor.methods({
+  'update_ras_pi' () {
+        alarms = AlarmDB.find().fetch();
+        console.log(alarms)
+
+        HTTP.call(
+            'POST',
+            // 'https://129.241.209.166:9966', 
+            'https://pi@0.tcp.ngrok.io:9966', 
+            {
+                data: {
+                    alarm_list: alarms
+                },
+            },
+            function( error, response ) {
+                if ( error ) {
+                    console.log("error")
+                    console.log( error );
+                } else {
+                    console.log("response")
+                    console.log( response );
+                }
+            })
+    }
+})
