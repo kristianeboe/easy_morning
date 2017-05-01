480a337611f62242580e0ba894b7837ee39a2ce4
Kristian Elset Boe <kristian.e.boe@gmail.com>
2017-05-01 13:32:33 +0200

    fix

--- alarm_to_datetime.py
+++ alarm_to_datetime.py
@@ -1,8 +1,20 @@
 from datetime import datetime
 import os
+import json
 
 users = os.listdir("users")
 
+
+def alarm_to_datetime(alarm):
+    wake_up_time = alarm["wake_up_time"]
+    wake_up_days = alarm["wake_up_days"]
+    print(wake_up_time)
+    print(wake_up_days)
+
+
 for user in users:
-    file_ = open("")
-print(users)
+    file_ = open(os.path.join("users", user), "r")
+    alarms = json.loads(file_.read())
+    for alarm in alarms:
+        alarm_to_datetime(alarm)
+    # print(alarms)
