0d6e67919ee172171af5e1c25e9f26bda1e6991c
Kristian Elset Boe <kristian.e.boe@gmail.com>
2017-05-01 19:06:05 +0200

    moved some files

---
+++ bathroom/alarm_to_datetime.py
@@ -0,0 +1,69 @@
+from datetime import datetime, time, timedelta
+import os
+import json
+
+users = os.listdir("users")
+
+
+def format_time(time):
+    if ":" in time:
+        return time[0:2] + time[3:]
+    else:
+        return time
+
+def alarms_to_datetime(alarm):
+    wake_up_time = alarm["wake_up_time"]
+    wake_up_days = format_time(alarm["wake_up_days"])
+    wake_up_hour = int(wake_up_time[0:2])
+    wake_up_minute = int(wake_up_time[3:4])
+
+
+    now = datetime.now().weekday()+1 if datetime.now().weekday() < 6 else 0
+
+    alarms = []
+
+    print(wake_up_hour, wake_up_minute)
+    for wake_up_day in wake_up_days:
+        delta = int(wake_up_day) - now
+
+        if delta < 0:
+            delta = delta + 7
+
+        if delta == 0:
+            print(datetime.now())
+            print(datetime.now().replace(hour=wake_up_hour, minute=wake_up_minute))
+            
+            if datetime.now() > datetime.now().replace(hour=wake_up_hour, minute=wake_up_minute):
+                alarm_ = datetime.now() + timedelta(days=7)
+                alarm_ =  datetime.now().replace(hour=wake_up_hour, minute=wake_up_minute, second=0, microsecond=0)
+            
+        else:
+            alarm_ = datetime.now() + timedelta(days=delta)
+            alarm_.replace(hour=wake_up_hour, minute=wake_up_minute, second=0, microsecond=0)
+        
+        alarms.append(alarm_)
+    
+    return alarms
+
+        # print("delta", delta)
+
+        # new_datetime = timedelta()
+
+        # print(new_datetime)
+    # print(now)
+
+
+user_to_alarm_datetime = {}
+for user in users:
+    # print(user)
+    file_ = open(os.path.join("users", user), "r")
+    alarms = json.loads(file_.read())
+    user_to_alarm_datetime[user] = []
+    for alarm in alarms:
+        alarms_datetime = alarms_to_datetime(alarm)
+        user_to_alarm_datetime[user].append([alarm_datetime.isoformat() for alarm_datetime in alarms_datetime])
+
+
+lol = json.dumps(user_to_alarm_datetime)
+print(lol)
+    # print(alarms)
