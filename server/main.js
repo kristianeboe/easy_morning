6e74c6a728e0f3ea03e2aa975e77d2c2ac235f50
Kristian Elset Boe <kristian.e.boe@gmail.com>
2017-05-01 19:03:59 +0200

    removed some files

--- server/main.js
+++ server/main.js
@@ -8,27 +8,7 @@ Meteor.startup(() => {
 Meteor.methods({
   'update_ras_pi' () {
         alarms = AlarmDB.find().fetch();
-        console.log(alarms)
 
-        // HTTP.call(
-        //     'POST',
-        //     // 'https://129.241.209.166:9966', 
-        //     // 'https://pi@0.tcp.ngrok.io:9966', 
-        //     'http://localhost:8080',
-        //     {
-        //         data: {
-        //             alarm_list: alarms
-        //         },
-        //     },
-        //     function( error, response ) {
-        //         if ( error ) {
-        //             console.log("error")
-        //             console.log( error );
-        //         } else {
-        //             console.log("response")
-        //             console.log( response );
-        //         }
-        //     })
         HTTP.post(
           // "http://localhost:9966",
           // "http://127.0.0.1:4040",
